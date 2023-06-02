540d76f710f47f8f26c9afaec2ce164468cbaa42
lniki _ <nik_ivanov_33@mail.ru>
2023-06-02 20:07:23 +0500

    Дописал представления для контракта и организации

--- Lab6new/RepresentationFactory/OrganisationTableRepresentation.cs
+++ Lab6new/RepresentationFactory/OrganisationTableRepresentation.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using Lab6new.Models;
+using Lab6new.RepresentationFactory.Interface;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lab6new.RepresentationFactory
 {
-    internal class OrganisationTableRepresentation
+    internal class OrganisationTableRepresentation : IOrganisationRepresentation
     {
+        [DisplayName("Название")]
+        public string FullName { get; }
+
+        [DisplayName("ИНН")]
+        public string Inn { get; }
+
+        [DisplayName("КПП")]
+        public string Kpp { get; }
+
+        [DisplayName("Адрес")]
+        public string Address { get; }
+
+        [DisplayName("Тип")]
+        public string Type { get; }
+
+        [DisplayName("Лицо")]
+        public string Entity { get; }
+
+        public Organisation Organisation { get; }
+
+        public OrganisationTableRepresentation(Organisation organisation)
+        {
+            Organisation = organisation;
+            FullName = organisation.FullName;
+            Inn = organisation.Inn;
+            Kpp = organisation.Kpp;
+            Address = organisation.Address;
+            Type = organisation.Type;
+            Entity = organisation.Entity ? "Физической" : "Юридическое";
+        }
     }
 }
